919ba1946d2fd4578b7acaf1dca0dd2d5a3c3063
Jupally,Hari Priya <s533990@ch140033m.nwmissouri.edu>
2019-01-31 10:18:27 -0600

    Initial Commit

---
+++ Hello Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Xcode
+//
+//  Created by Jupally,Hari Priya on 1/31/19.
+//  Copyright © 2019 Jupally,Hari Priya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
